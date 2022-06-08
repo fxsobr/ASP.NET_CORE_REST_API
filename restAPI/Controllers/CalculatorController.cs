af61db731981a1b64128cec56deafb7efc91d378
Marcelo Ceolin <fxsobr@gmail.com>
2022-06-08 20:34:11 -0300

    Projeto inicial

---
+++ restAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace restAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CalculatorController : ControllerBase
+{
+    private readonly ILogger<CalculatorController> _logger;
+
+    public CalculatorController(ILogger<CalculatorController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("sum/{firstNumber}/{secondNumber}")]
+    public IActionResult Get()
+    {
+        return BadRequest("Invalid input");
+    }
+
+}
