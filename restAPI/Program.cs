f4107e19722473ed1b095df4776588e31a182fcf
Marcelo Ceolin <fxsobr@gmail.com>
2022-06-09 20:58:25 -0300

    finalizado restApi mockada

--- restAPI/Program.cs
+++ restAPI/Program.cs
@@ -1,3 +1,6 @@
+using restAPI.Services;
+using restAPI.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
 
 var app = builder.Build();
 
