87751ec0d67e9d89aad893d9e4a310da4552a3dd
Marcelo Ceolin <fxsobr@gmail.com>
2022-06-09 20:09:43 -0300

    Implementando operações matemáticas

--- restAPI/Controllers/CalculatorController.cs
+++ restAPI/Controllers/CalculatorController.cs
@@ -15,12 +15,45 @@ public class CalculatorController : ControllerBase
     }
 
     [HttpGet("sum/{firstNumber}/{secondNumber}")]
-    public IActionResult Get(string firstNumber, string secondNumber)
+    public IActionResult GetSum(string firstNumber, string secondNumber)
     {
         if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
         {
             var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
             return Ok(sum.ToString(CultureInfo.CurrentCulture));
+        }
+        return BadRequest("Invalid input");
+    }
+    
+    [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+    public IActionResult GetSubtraction(string firstNumber, string secondNumber)
+    {
+        if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        {
+            var sub = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+            return Ok(sub.ToString(CultureInfo.CurrentCulture));
+        }
+        return BadRequest("Invalid input");
+    }
+    
+    [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+    public IActionResult GetMultiplication(string firstNumber, string secondNumber)
+    {
+        if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        {
+            var mul = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+            return Ok(mul.ToString(CultureInfo.CurrentCulture));
+        }
+        return BadRequest("Invalid input");
+    }
+    
+    [HttpGet("division/{firstNumber}/{secondNumber}")]
+    public IActionResult GetDivision(string firstNumber, string secondNumber)
+    {
+        if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        {
+            var div = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+            return Ok(div.ToString(CultureInfo.CurrentCulture));
         }
         return BadRequest("Invalid input");
     }
